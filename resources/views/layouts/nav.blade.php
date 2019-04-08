17ffffcb1b314ec67914c33c72f1e533785471e7
Ryan McDonagh <ryan@henschen.com>
2019-04-08 17:03:19 -0400

    worked on getting locked threads to work after adding channelAdmin

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
+
             <ul class="nav navbar-nav">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse <span class="caret"></span></a>
